cb953c08d634a211f24e6ec34040f8b698c647b4
mgmilton <matt.milton@gmail.com>
2017-12-13 11:13:40 -0700

    Adds Tests

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -6,8 +6,13 @@ require './lib/joke'
 class JokeTest < Minitest::Test
 
   def test_joke_instantiates
-    joke = Joke.new
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
     assert_instance_of Joke, joke
+  end
+
+  def test_it_has_an_id
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    assert_equal 1, joke.id
   end
 
 end
